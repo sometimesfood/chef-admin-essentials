8bb3e8863310dd75ef44b3cd0dc1fa96039c942d
Sebastian Boehm <sebastian@sometimesfood.org>
2010-10-24 15:18:27 +0200

    Remove hardcoded cookbook name

--- libraries/admin_essentials.rb
+++ libraries/admin_essentials.rb
@@ -2,4 +2,8 @@ module AdminEssentials
   def packages_include?(p)
     return node.admin_essentials.all_packages.include? p
   end
+
+  def include_recipe_relative(recipe)
+    include_recipe "#{cookbook_name}::#{recipe}"
+  end
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -13,12 +13,12 @@ node.admin_essentials.all_packages.each do | a |
   package a
 end
 
-include_recipe 'admin-essentials::etckeeper'
-include_recipe 'admin-essentials::ack'
-include_recipe 'admin-essentials::mc'
-include_recipe 'admin-essentials::ssh'
-include_recipe 'admin-essentials::molly-guard'
-include_recipe 'admin-essentials::grml-zsh'
+include_recipe_relative 'etckeeper'
+include_recipe_relative 'ack'
+include_recipe_relative 'mc'
+include_recipe_relative 'ssh'
+include_recipe_relative 'molly-guard'
+include_recipe_relative 'grml-zsh'
 
 if packages_include? "zsh"
   user "root" do
