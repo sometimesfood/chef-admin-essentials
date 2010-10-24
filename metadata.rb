f23fb9ccd92773de97b301449cdceb159939d022
Sebastian Boehm <sebastian@sometimesfood.org>
2010-10-24 18:09:13 +0200

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,9 @@ maintainer       "Sebastian Boehm"
 maintainer_email "sebastian@sometimesfood.org"
 license          "MIT"
 description      "Install essential admin tools"
-#long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 #version          "0.1"
+
+%w(ubuntu debian).each do |os|
+  supports os
+end
