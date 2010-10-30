8ad4e20ff9c21602057ff4d231340c54b980e49a
Sebastian Boehm <sebastian@sometimesfood.org>
2010-10-30 18:06:58 +0200

    Clean up install-chef.sh output

--- contrib/install-chef.sh
+++ contrib/install-chef.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-echo 'deb http://apt.opscode.com/ lucid main' | sudo tee /etc/apt/sources.list.d/opscode.list
-wget -qO - "http://apt.opscode.com/packages@opscode.com.gpg.key" | sudo apt-key add -
-sudo aptitude update
-sudo DEBIAN_FRONTEND=noninteractive aptitude -y -q install chef
-sudo update-rc.d -f chef-client remove
+keyserver=subkeys.pgp.net
+opscode_key_id=83EF826A
+
+echo 'Adding Opscode repo to sources.list.d...'
+echo 'deb http://apt.opscode.com/ lucid main' | sudo tee /etc/apt/sources.list.d/opscode.list > /dev/null
+echo 'Adding Opscode key to trusted keyring...'
+sudo apt-key adv --keyserver ${keyserver} --recv-keys ${opscode_key_id} 2>&1 > /dev/null
+echo 'Updating package lists (this may take a while)...'
+sudo aptitude update > /dev/null
+echo 'Installing chef...'
+sudo DEBIAN_FRONTEND=noninteractive aptitude -y -q install chef > /dev/null
+echo 'Removing chef-client from default runlevel...'
+sudo update-rc.d -f chef-client remove > /dev/null
