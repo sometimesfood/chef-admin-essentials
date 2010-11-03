5c5374cfedd7639abf0083375f1e3864fc0424d9
Sebastian Boehm <sebastian@sometimesfood.org>
2010-11-03 18:44:53 +0100

    Hide message about chef-client failing to start in install-chef.sh

--- contrib/install-chef.sh
+++ contrib/install-chef.sh
@@ -3,12 +3,16 @@ keyserver=subkeys.pgp.net
 opscode_key_id=83EF826A
 
 echo 'Adding Opscode repo to sources.list.d...'
-echo 'deb http://apt.opscode.com/ lucid main' | sudo tee /etc/apt/sources.list.d/opscode.list > /dev/null
+echo 'deb http://apt.opscode.com/ lucid main' \
+  | sudo tee /etc/apt/sources.list.d/opscode.list > /dev/null
 echo 'Adding Opscode key to trusted keyring...'
-sudo apt-key adv --keyserver ${keyserver} --recv-keys ${opscode_key_id} 2>&1 > /dev/null
+sudo apt-key adv --keyserver ${keyserver} --recv-keys ${opscode_key_id} > /dev/null
 echo 'Updating package lists (this may take a while)...'
-sudo aptitude update > /dev/null
+sudo apt-get update > /dev/null
 echo 'Installing chef...'
-sudo DEBIAN_FRONTEND=noninteractive aptitude -y -q install chef > /dev/null
+sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install chef &> /dev/null \
+  || echo 'Installation failed.' \
+  && echo 'Please run "apt-get install chef" to see what went wrong.' \
+  && exit 1
 echo 'Removing chef-client from default runlevel...'
 sudo update-rc.d -f chef-client remove > /dev/null
