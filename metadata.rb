837158733c82ad80100365ae5d2d35e0c591639c
Sebastian Boehm <sebastian@sometimesfood.org>
2010-11-28 20:39:18 +0100

    Add recipe descriptions to metadata

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,14 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 #version          "0.1"
 depends          "apt-repo"
 
+recipe "admin-essentials",                   "Install essential admin tools"
+recipe "admin-essentials::ack",              "Install ack"
+recipe "admin-essentials::etckeeper",        "Set up etckeeper"
+recipe "admin-essentials::grml-zsh",         "Set up grml console config"
+recipe "admin-essentials::mc",               "Set up midnight commander"
+recipe "admin-essentials::molly-guard",      "Set up molly-guard"
+recipe "admin-essentials::root-preferences", "Set some preferences for root"
+
 %w(ubuntu debian).each do |os|
   supports os
 end
