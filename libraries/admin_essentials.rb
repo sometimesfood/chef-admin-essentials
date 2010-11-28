06628b730e59642a4b6eeecad2ca38aec3366177
Sebastian Boehm <sebastian@sometimesfood.org>
2010-11-28 23:20:44 +0100

    Add some recipe comments, remove an unneeded helper method

--- libraries/admin_essentials.rb
+++ libraries/admin_essentials.rb
@@ -1,8 +1,4 @@
 module AdminEssentials
-  def packages_include?(p)
-    return node.admin_essentials.all_packages.include? p
-  end
-
   def include_recipe_relative(recipe)
     include_recipe "#{cookbook_name}::#{recipe}"
   end
--- recipes/grml-zsh.rb
+++ recipes/grml-zsh.rb
@@ -1,5 +1,7 @@
+# add deb.grml.org apt repo sources
 include_recipe "apt-repo::grml"
 
+# make sure not to install anything but the packages we want
 directory "/etc/apt/preferences.d"
 cookbook_file "grml-pin" do
   path "/etc/apt/preferences.d/grml-pin"
@@ -9,6 +11,7 @@ end
 
 package "grml-etc-core"
 
+# only use screenrc_generic for now -- there's no grml-scripts-core package yet
 grml_screenrcs = ["/etc/grml/screenrc", "/etc/grml/screenrc_grml"]
 grml_screenrcs.each do | screenrc |
   file screenrc do
