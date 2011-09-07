18aa65c4ad760847c2d05f460ddce7349ca9e830
Sebastian Boehm <sebastian@sometimesfood.org>
2011-09-07 16:39:21 +0200

    Update recipe descriptions in metadata

--- metadata.rb
+++ metadata.rb
@@ -6,12 +6,14 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 #version          "0.1"
 depends          "apt-repo"
 
-recipe "admin-essentials",                   "Install essential admin tools"
-recipe "admin-essentials::ack",              "Install ack"
-recipe "admin-essentials::etckeeper",        "Set up etckeeper"
-recipe "admin-essentials::grml-zsh",         "Set up grml console config"
-recipe "admin-essentials::molly-guard",      "Set up molly-guard"
-recipe "admin-essentials::root-preferences", "Set some preferences for root"
+recipe "admin-essentials",                    "Install essential admin tools"
+recipe "admin-essentials::ack",               "Install ack"
+recipe "admin-essentials::admin-preferences", "Set some preferences for root"
+recipe "admin-essentials::emacs",             "Install emacs"
+recipe "admin-essentials::etckeeper",         "Set up etckeeper"
+recipe "admin-essentials::git",               "Set up git"
+recipe "admin-essentials::grml-zsh",          "Set up grml console config"
+recipe "admin-essentials::molly-guard",       "Set up molly-guard"
 
 grouping "admin-essentials",
   :display_name => "admin-essentials",
