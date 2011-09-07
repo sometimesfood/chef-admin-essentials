29d033cb9ac484158f7525422ccd520d8908d53d
Sebastian Boehm <sebastian@sometimesfood.org>
2011-09-07 16:36:40 +0200

    Add attribute descriptions to metadata

--- metadata.rb
+++ metadata.rb
@@ -13,6 +13,37 @@ recipe "admin-essentials::grml-zsh",         "Set up grml console config"
 recipe "admin-essentials::molly-guard",      "Set up molly-guard"
 recipe "admin-essentials::root-preferences", "Set some preferences for root"
 
+grouping "admin-essentials",
+  :display_name => "admin-essentials",
+  :description => "admin-essentials attribute settings",
+  :type => "hash"
+
+attribute "admin-essentials/packages",
+  :display_name => "admin-essentials packages",
+  :description => "admin packages to install",
+  :type => "array",
+  :calculated => true,
+  :required => "optional"
+
+attribute "admin-essentials/admin-users",
+  :display_name => "admin-essentials admin users",
+  :description => "users to set admin preferences for (apart from root)",
+  :type => "array",
+  :required => "optional"
+
+attribute "admin-essentials/admin-groups",
+  :display_name => "admin-essentials admin groups",
+  :description => "groups to set admin preferences for (apart from root)",
+  :type => "array",
+  :required => "optional"
+
+attribute "admin-essentials/admin-groups",
+  :display_name => "admin-essentials editor",
+  :description => "$EDITOR to set for admins",
+  :type => "string",
+  :calculated => true,
+  :required => "optional"
+
 %w(ubuntu debian).each do |os|
   supports os
 end
