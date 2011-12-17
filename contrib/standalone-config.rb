9f570fbf9565379f9da6e22a46009dbefec4fc81
Sebastian Boehm <sebastian@sometimesfood.org>
2011-12-17 16:26:14 +0100

    Disable verbose logging for standalone deployment

--- contrib/standalone-config.rb
+++ contrib/standalone-config.rb
@@ -1,4 +1,5 @@
 cookbooks = File.expand_path(File.join(File.dirname(__FILE__), "../.."))
 runlist = File.join(File.expand_path(File.dirname(__FILE__)), "standalone-runlist.json")
+verbose_logging nil
 cookbook_path cookbooks
 json_attribs runlist
