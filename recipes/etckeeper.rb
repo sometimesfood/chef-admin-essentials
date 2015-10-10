cd98b48551dc3a22ff288f35fb337ee1f31f0baa
Sebastian Boehm <sebastian@sometimesfood.org>
2015-10-10 21:08:53 +0200

    Remove "etckeeper init" task
    The etckeeper Debian package runs "etckeeper init" as part of the postinst script.

--- recipes/etckeeper.rb
+++ recipes/etckeeper.rb
@@ -5,14 +5,3 @@ package "etckeeper"
 cookbook_file "/etc/etckeeper/etckeeper.conf" do
   source "etckeeper.conf"
 end
-
-# initialize etckeeper if /etc is not already a git repository
-execute "etckeeper init" do
-  not_if { File.exists? "/etc/.git" }
-end
-
-# commit /etc if we have just initialized etckeeper
-execute "etckeeper commit 'Initial commit, triggered by chef-admin-essentials'" do
-  action :nothing
-  subscribes :run, resources(:execute => "etckeeper init"), :immediately
-end
