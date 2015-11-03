db592ad724aebf3c27b3762ddd8411c3a3e077f5
Sebastian Boehm <sebastian@sometimesfood.org>
2015-11-03 17:56:22 +0100

    Fix etckeeper install

--- recipes/etckeeper.rb
+++ recipes/etckeeper.rb
@@ -1,6 +1,8 @@
 include_recipe_relative "git"
 
-package "etckeeper"
+package "etckeeper" do
+  options '-o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"'
+end
 
 cookbook_file "/etc/etckeeper/etckeeper.conf" do
   source "etckeeper.conf"
